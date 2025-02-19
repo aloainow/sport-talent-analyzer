d33dd5bbf6601842abc43b42110591b240a9bab7
aloainow <149620618+aloainow@users.noreply.github.com>
2025-02-19 09:10:29 -0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,17 @@
 # utils/__init__.py
-# Deixe vazio por enquanto ou apenas importe o que for necessário
-from .openai_helper import get_sport_recommendations
-from .test_processor import process_test_results, normalize_score, calculate_average
 
+# Importações das funções necessárias
+from .openai_helper import get_sport_recommendations
+from .test_processor import (
+    process_test_results,
+    normalize_score,
+    calculate_average
+)
+
+# Definição do que será exportado
 __all__ = [
     'get_sport_recommendations',
-    'process_test_results', 
+    'process_test_results',
     'normalize_score',
     'calculate_average'
 ]
