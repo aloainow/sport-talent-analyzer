35c2eb4b6ee50888c7273736cbdb33525f77f3c4
aloainow <149620618+aloainow@users.noreply.github.com>
2025-02-20 10:57:16 -0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,1 @@
-# Importações dos módulos
-from .test_processor import normalize_score, calculate_average, process_test_results
-from .openai_helper import get_sport_recommendations
-
-__all__ = [
-    'get_sport_recommendations',
-    'process_test_results',
-    'normalize_score',
-    'calculate_average'
-]
-
-__version__ = '1.0.0'
+# Empty init file to mark directory as Python package
