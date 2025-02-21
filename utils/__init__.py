1a92b810cb2289c5d760ef65ed974a7a19cfd7fe
aloainow <149620618+aloainow@users.noreply.github.com>
2025-02-21 19:43:56 -0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,39 +1,13 @@
-from .sport_helper import (
-    get_sport_recommendations,
-    calculate_biotype_compatibility,
-    calculate_physical_compatibility,
-    get_sport_strengths,
-    get_development_areas,
-    translate_sport_name,
-    load_and_process_data
-)
-
-from .test_processor import (
-    normalize_score,
-    calculate_average,
-    process_test_results
-)
-
-from .age_adjusted_calculations import (
-    get_age_group,
-    calculate_age_adjusted_score,
-    get_development_potential,
-    calculate_final_score
-)
+# Importações simplificadas
+from .sport_helper import get_sport_recommendations
+from .test_processor import process_test_results, normalize_score, calculate_average
+from .age_adjusted_calculations import get_age_group, calculate_age_adjusted_score
 
 __all__ = [
     'get_sport_recommendations',
-    'calculate_biotype_compatibility',
-    'calculate_physical_compatibility',
-    'get_sport_strengths',
-    'get_development_areas',
-    'translate_sport_name',
-    'load_and_process_data',
+    'process_test_results',
     'normalize_score',
     'calculate_average',
-    'process_test_results',
     'get_age_group',
-    'calculate_age_adjusted_score',
-    'get_development_potential',
-    'calculate_final_score'
+    'calculate_age_adjusted_score'
 ]
