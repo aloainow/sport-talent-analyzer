1704b6ef8c21b40b4cacee83957a8af0ae0a4283
aloainow <149620618+aloainow@users.noreply.github.com>
2025-02-21 19:45:47 -0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,13 +1,1 @@
-# Importações simplificadas
-from .sport_helper import get_sport_recommendations
-from .test_processor import process_test_results, normalize_score, calculate_average
-from .age_adjusted_calculations import get_age_group, calculate_age_adjusted_score
 
-__all__ = [
-    'get_sport_recommendations',
-    'process_test_results',
-    'normalize_score',
-    'calculate_average',
-    'get_age_group',
-    'calculate_age_adjusted_score'
-]
