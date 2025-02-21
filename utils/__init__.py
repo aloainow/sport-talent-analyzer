b3239e213c3a1673b2b38b2dea0997952bb4c231
aloainow <149620618+aloainow@users.noreply.github.com>
2025-02-21 18:48:43 -0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,21 @@
-# Empty init file to mark directory as Python package
+# Efrom .sport_helper import *
+from .test_processor import *
+from .age_adjusted_calculations import *
+
+__all__ = [
+    'get_sport_recommendations',
+    'get_recommendations_without_api',
+    'calculate_biotype_compatibility',
+    'calculate_physical_compatibility',
+    'get_sport_strengths',
+    'get_development_areas',
+    'translate_sport_name',
+    'load_and_process_data',
+    'normalize_score',
+    'calculate_average',
+    'process_test_results',
+    'get_age_group',
+    'calculate_age_adjusted_score',
+    'get_development_potential',
+    'calculate_final_score'
+]mpty init file to mark directory as Python package
